3e744c554da2dd5620ca5dd867cdb8b55f2380c7
sudip6164 <sudippradhanadgj@gmail.com>
2025-09-06 15:38:27 +0545

    create resume

--- accounts/forms.py
+++ accounts/forms.py
@@ -35,6 +35,12 @@ class UserRegistrationForm(UserCreationForm):
         model = User
         fields = ('username', 'email', 'password1', 'password2')
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '').strip().lower()
+        if User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError('A user with this email already exists.')
+        return email
+
     def save(self, commit=True):
         user = super().save(commit=False)
         user.email = self.cleaned_data['email']
